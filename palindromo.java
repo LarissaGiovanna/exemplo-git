7dd898f71ec04e8e9e3b7263f6b124b9154399c4
LarissaGiovanna <lgiova544@gmail.com>
2024-08-01 16:27:26 -0300

    exercicio palindromo java

---
+++ palindromo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class palindromo {
+    public static void main(String[] args) {
+        String palavra, inverso = "";
+        System.out.println("Digite a palavra:");
+        Scanner s = new Scanner(System.in);
+        palavra = s.next();
+        
+        for (int i = palavra.length() - 1; i >= 0; i--) {
+            inverso = inverso+palavra.charAt(i);
+        }
+
+        if(palavra.equals(inverso)){
+            System.out.println("A palavra é um palindromo");
+        }else{
+            System.out.println("A palavra digitada não é um palindromo.");
+        }
+    }
+}
